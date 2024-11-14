e6566a57c361c1e675968760c357dc88ac6bbb33
ximenareza <166732925+ximenareza@users.noreply.github.com>
2024-11-14 02:00:49 +0000

    ⭐Entregando respuesta

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ app.use((req, res, next)=>{
   next();
 });
 
+// Registrando el segundo middleware
+app.use((req, res, next)=>{
+  console.log("📢 Middleware #2");
+  // Se contesta al server 
+  res.send(`
+    <h1>Welcome to Express Js</h1>
+    <p>This is my awesome app! 😎</p>
+  `);
+});
+
 // Asignando el middleware de express
 // al servidor http
 const server = http.createServer(app);
