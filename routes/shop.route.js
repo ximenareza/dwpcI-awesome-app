1a4c8a04ea970008248a6aa8ff5308fec17c5958
ximenareza <166732925+ximenareza@users.noreply.github.com>
2024-12-08 02:27:30 +0000

    ⭐Renderizado condicional

--- routes/shop.route.js
+++ routes/shop.route.js
@@ -11,10 +11,12 @@ router.get('/', (req, res)=>{
   // Mostrando productos en memoria
   console.log(products);
   console.log("📢 Sirviendo la ruta '/'");
+  console.log(`length: ${ products.length === 0 }`);
   res.render('shop', { 
     shop: 'active', 
     docTitle:"Shop",
     viewStyle: '/css/product.css',
+    isProductsListEmpty: products.length === 0,
     products
   });
 });
