5a03979512130946a34bc0e2b67c5dd59973c750
ximenareza <166732925+ximenareza@users.noreply.github.com>
2024-12-08 03:00:00 +0000

    ⭐Patron MVC

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,35 +1,16 @@
 // Importando el enrutador de express
 import { Router } from 'express';
-// Importando el gestor de rutas
-import path from 'path';
-import { ROOT_DIR } from '../helpers/paths.js';
+// Importando funciones del controlador
+import { getAddProduct, postAddProduct } from '../controllers/products.controller.js'
 
 // Creando una instancia del enrutador de express
 const router = Router();
 
-// Datos en memoria volatil
-export const products = [];
-
 // GET /admin/add-product
-router.get('/add-product', (req, res, next) => {
-  // Servimos el formulario
-  console.log("📢 Sirviendo formulario...");
-  res.render('add-product', {
-    addProduct: `active`, 
-    viewStyle: '/css/add-product.css',
-    docTitle:"Add Product"});
-});
+router.get('/add-product', getAddProduct);
 
 // POST /admin/add-product
-router.post('/add-product', (req, res) => {
-  // Realizaremos la desestructuracion de
-  // "name" de la petición
-  const { title } = req.body;
-  // Agregamos el dato en la base de datos
-  products.push(title);
-  // Redireccionando
-  res.redirect('/');
-});
+router.post('/add-product', postAddProduct);
 
 // Exportando el enrutador admin
 export default router;
--- routes/shop.route.js
+++ routes/shop.route.js
@@ -1,25 +1,13 @@
 // Importando el enrutador de express
 import { Router } from 'express';
-// Importando el arreglo de productos
-import { products } from './admin.route.js';
+// Importando Action funcion del controlador products
+import { getProducts } from '../controllers/products.controller.js'
 
 // Creando una instancia del enrutador de express
 const router = Router();
 
 // GET /
-router.get('/', (req, res)=>{
-  // Mostrando productos en memoria
-  console.log(products);
-  console.log("📢 Sirviendo la ruta '/'");
-  console.log(`length: ${ products.length === 0 }`);
-  res.render('shop', { 
-    shop: 'active', 
-    docTitle:"Shop",
-    viewStyle: '/css/product.css',
-    isProductsListEmpty: products.length === 0,
-    products
-  });
-});
+router.get('/', getProducts);
 
 // GET /about
 router.get('/about', (req, res) => {
